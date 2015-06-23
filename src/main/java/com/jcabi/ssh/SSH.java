fbe49ef570cb80e3010dd57c378d23ebfbf68561
Suseika <wlasowegor@gmail.com>
2015-06-24 02:07:42 +0500

    Remove a line PDD was complaining about

--- src/main/java/com/jcabi/ssh/SSH.java
+++ src/main/java/com/jcabi/ssh/SSH.java
@@ -66,7 +66,6 @@ import org.apache.commons.lang3.Validate;
  *
  * <p>It is highly recommended to use classes from {@link Shell} interface,
  * they will simplify operations.</p>
- *
  * @todo #21:30min This class has common data, implementation of exec() and
  * constructor validations with SSHByPassword class. Common functionality
  * should be extracted into a separate module.
